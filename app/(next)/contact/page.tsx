8748a512017a7c4fd2663563f9fbd612f2bc7086
Thibaut Narivelo <thibaut.randria@gmail.com>
2024-06-08 18:19:44 +0200

    feat(email): contact page

--- app/(next)/contact/page.tsx
+++ app/(next)/contact/page.tsx
@@ -1,26 +1,14 @@
-import { Resend } from "resend";
-import EmailTemplate from "../../(next)/components/email-template";
+import ContactForm from "../components/contact-form";
 
-export default async function Page() {
-  async function send() {
-    "use server";
-    const resend = new Resend(process.env.RESEND_API_KEY);
-
-    const { data, error } = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["thibaut.randria@gmail.com"],
-      subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }),
-    });
-    console.log(data);
-  }
+export default function Contact() {
   return (
-    <form action={send}>
-      {/* <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <textarea placeholder="Message"></textarea>
-        <input type="file" /> */}
-      <button type="submit">Submit</button>
-    </form>
+    <section className="py-24">
+      <div className="container">
+        <h1 className="mb-10 text-center font-serif text-3xl font-medium">
+          Contact us
+        </h1>
+        <ContactForm />
+      </div>
+    </section>
   );
 }
