29264f93f122f10f5469b6f391b8f160c1b9887b
Thibaut Narivelo <thibaut.randria@gmail.com>
2024-06-13 12:21:14 +0200

    feat(routes): parent route content

--- app/(next)/[pages]/page.tsx
+++ app/(next)/[pages]/page.tsx
@@ -24,14 +24,13 @@ export default async function Page({ params }: Props) {
 
   return (
     <div className="container mx-auto min-h-screen px-5">
-      {content.sections?.map((section) => (
+      {/* {content.sections?.map((section) => (
         <div key={section.title} className="ml-[20%] flex flex-col">
           <h1 className="pb-[2rem] text-[3rem] font-bold uppercase leading-[2.5rem] tracking-tighter">
             {section.title}
           </h1>
           {section.content?.map((block) => (
             <div key={block.title}>
-              {/*  Text Content block type */}
               <div>
                 {block._type.includes("customText") && (
                   <>
@@ -40,7 +39,6 @@ export default async function Page({ params }: Props) {
                 )}
               </div>
 
-              {/*  customImage block type */}
               <div>
                 {block._type.includes("customImage") && (
                   <>
@@ -53,7 +51,6 @@ export default async function Page({ params }: Props) {
                 )}
               </div>
 
-              {/*  customExternalLink block type */}
               <div className="flex">
                 {block._type.includes("customExternalLink") && (
                   <a
@@ -69,7 +66,50 @@ export default async function Page({ params }: Props) {
             </div>
           ))}
         </div>
-      ))}
+      ))} */}
+      {/* display only the first content from content.sections */}
+      <div className="ml-[20%] flex flex-col">
+        <h1 className="pb-[2rem] text-[3rem] font-bold uppercase leading-[2.5rem] tracking-tighter">
+          {content.sections && content.sections[0]?.title}
+        </h1>
+        {content.sections &&
+          content.sections[0]?.content?.map((block) => (
+            <div key={block.title}>
+              <div>
+                {block._type.includes("customText") && (
+                  <>
+                    <p>{block.content}</p>
+                  </>
+                )}
+              </div>
+
+              <div>
+                {block._type.includes("customImage") && (
+                  <>
+                    <CustomImage
+                      imageUrl={block.imageUrl || ""}
+                      title={block.title || ""}
+                    />
+                    <span>{block.url}</span>
+                  </>
+                )}
+              </div>
+
+              <div className="flex">
+                {block._type.includes("customExternalLink") && (
+                  <a
+                    href={block.url || ""}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="-"
+                  >
+                    {block.title}
+                  </a>
+                )}
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
