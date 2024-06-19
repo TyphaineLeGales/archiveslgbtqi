e698a232c9a194868bf745b6ef33e18f2d2c0757
Thibaut Narivelo <thibaut.randria@gmail.com>
2024-06-19 19:53:28 +0200

    refactor: Update SingleEvent component transition and styling
    - Adjust transition duration and ease for SingleEvent component
    - Add z-index to overlay background in SingleEvent component
    - Update border and padding styles for event title container in SingleEvent component

--- app/(next)/components/events/SingleEvent.tsx
+++ app/(next)/components/events/SingleEvent.tsx
@@ -18,10 +18,10 @@ export default function SingleEvent({ params, event }: Event) {
         initial={{ translateY: "-50vh" }}
         animate={{ translateY: "-100vh" }}
         transition={{ duration: 1.5, ease: [0.6, 0.01, 0.05, 0.95] }}
-        className="fixed inset-0 bg-neutral-700"
+        className="fixed inset-0 z-50 bg-neutral-700"
       />
-      <div className="flex justify-between px-[1rem]">
-        <h1 className="eventTitle">{event?.eventTitle}</h1>
+      <div className="flex justify-between border-b-[1px] border-black px-[1rem]">
+        <h1 className="eventTitle truncate">{event?.eventTitle}</h1>
         <div className="h-fit rounded-full border-[1px] border-black px-[1rem] py-[.25rem] text-[.75rem]">
           <DateFormat dateString={event?.eventDate?.eventStartDate || ""} />
           {event?.eventDate?.addEndDate && (
