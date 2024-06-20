3c30d7ce239f9373f16e86f4732d7a2465152869
Thibaut Narivelo <thibaut.randria@gmail.com>
2024-06-20 09:24:52 +0200

    feat: Update SingleEvent and EventsDesktop components
    - Adjust transition duration and ease for SingleEvent component
    - Add z-index to overlay background in SingleEvent component
    - Update border and padding styles for event title container in SingleEvent component
    - Update CSS class names for background color in EventsDesktop component
    - Update CSS class names for background color in SingleEvent component
    - Adjust transition duration and ease in EventsDesktop component
    - Adjust transition duration and ease in SingleEvent component

--- app/(next)/components/events/SingleEvent.tsx
+++ app/(next)/components/events/SingleEvent.tsx
@@ -20,17 +20,20 @@ export default function SingleEvent({ params, event }: Event) {
         transition={{ duration: 1.5, ease: [0.6, 0.01, 0.05, 0.95] }}
         className="fixed inset-0 z-50 bg-neutral-700"
       />
-      <div className="flex justify-between border-b-[1px] border-black px-[1rem]">
-        <h1 className="eventTitle truncate">{event?.eventTitle}</h1>
-        <div className="h-fit rounded-full border-[1px] border-black px-[1rem] py-[.25rem] text-[.75rem]">
-          <DateFormat dateString={event?.eventDate?.eventStartDate || ""} />
-          {event?.eventDate?.addEndDate && (
-            <>
-              <span className="mx-[0.5rem]">-</span>
-              <DateFormat dateString={event.eventDate.eventEndDate || ""} />
-            </>
-          )}
+      <div className="">
+        <div className="flex justify-between border-b-[1px] border-black px-[1rem]">
+          <h1 className="eventTitle w-2/3 truncate">{event?.eventTitle}</h1>
+          <div className="h-fit w-1/3 rounded-full border-[1px] border-black px-[1rem] py-[.25rem] text-[.75rem]">
+            <DateFormat dateString={event?.eventDate?.eventStartDate || ""} />
+            {event?.eventDate?.addEndDate && (
+              <div>
+                <span className="mx-[0.5rem]">-</span>
+                <DateFormat dateString={event.eventDate.eventEndDate || ""} />
+              </div>
+            )}
+          </div>
         </div>
+        <p>{event?.eventDescription}</p>
       </div>
     </motion.div>
   );
