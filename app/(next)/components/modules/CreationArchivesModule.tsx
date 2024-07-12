ab5704a1c7f31894046f7acf7b3caa1d615e54a3
Thibaut Narivelo <thibaut.randria@gmail.com>
2024-07-12 22:23:01 +0200

    feat: drawer component

--- app/(next)/components/modules/CreationArchivesModule.tsx
+++ app/(next)/components/modules/CreationArchivesModule.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import clsx from "clsx";
 
 import { PortableTextBlock } from "next-sanity";
 
@@ -17,20 +19,47 @@ export default function CreationArchivesModule({
   intro,
   archive,
 }: CreationArchivesModuleProps) {
+  const [archiveIndex, setArchiveIndex] = useState<number | null>(null);
+
+  const handleClick = (index: number) => {
+    if (archiveIndex === index) {
+      setArchiveIndex(null);
+    } else {
+      setArchiveIndex(index);
+    }
+  };
   return (
     <div className="flex flex-col gap-[1rem] py-[1rem]">
       {/* TODO: Collapsible */}
       <MyCustomPortableText value={intro} />
-      <ul className="divide-y-[1px] divide-black/10 border-t-[1px] border-black">
+      <ul className="divide-y-[1px] divide-black border-t-[1px] border-black will-change-transform">
         {archive?.map((archiveItem, index) => (
-          <li
-            key={`archive-item-${index}`}
-            className="flex flex-col gap-[1rem] py-[2rem]"
+          <button
+            key={index}
+            onClick={() => handleClick(index)}
+            className={clsx(
+              "relative flex w-full flex-col items-start justify-start overflow-hidden py-[1rem] text-start transition-[max-height] duration-[.5s] ease-in-out",
+              archiveIndex === index ? "max-h-[15rem]" : "max-h-[2rem]",
+            )}
           >
-            <h3>{archiveItem.title}</h3>
-            <MyCustomPortableText value={archiveItem.description} />
-            <p>{archiveItem.status}</p>
-          </li>
+            <div className="absolute left-0 top-[.25rem] flex w-full items-center justify-between pr-[1rem]">
+              <h3 className="text-[1.5rem] font-bold leading-[1.5rem] tracking-tighter">
+                {archiveItem.title}
+              </h3>
+              <div
+                className={clsx(
+                  "font-bold transition-transform duration-[.5s] ease-tamisitée",
+                  archiveIndex === index ? "rotate-45" : "rotate-0",
+                )}
+              >
+                +
+              </div>
+            </div>
+            <li className="pt-[1rem]">
+              <MyCustomPortableText value={archiveItem.description} />
+              <p>{archiveItem.status}</p>
+            </li>
+          </button>
         ))}
       </ul>
     </div>
