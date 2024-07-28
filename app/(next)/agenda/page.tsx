75643758b9ad23ec3c45ff2e57e7ef9c23839264
Thibaut Narivelo <thibaut.randria@gmail.com>
2024-07-28 17:06:41 +0200

    refactor: Update agenda page components and styles

--- app/(next)/agenda/page.tsx
+++ app/(next)/agenda/page.tsx
@@ -5,11 +5,7 @@ import { sanityFetch } from "@/sanity/lib/fetch";
 import { lastEventQuery, pastEventQuery } from "@/sanity/lib/queries";
 import { LastEventQueryResult, PastEventQueryResult } from "@/sanity.types";
 
-import {
-  AgendaDesktopSidebar,
-  FutureEvents,
-  PastEvents,
-} from "../components/agenda";
+import { AgendaDesktopSidebar, EventsMap } from "../components/agenda";
 
 export default async function Page() {
   const [pastEvent, futureEvent] = await Promise.all([
@@ -26,8 +22,8 @@ export default async function Page() {
     <div className="relative mx-auto mt-[3rem] min-h-screen pb-[1rem] lg:max-w-[1440px]">
       <AgendaDesktopSidebar />
       <div className="flex flex-col gap-[5rem] pr-[3rem] lg:ml-arch">
-        <FutureEvents id="future" futureEvent={futureEvent} />
-        <PastEvents id="past" pastEvent={pastEvent} />
+        <EventsMap id="future" eventsMap={futureEvent} />
+        <EventsMap id="past" eventsMap={pastEvent} />
       </div>
     </div>
   );
