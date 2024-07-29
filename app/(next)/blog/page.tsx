ae3d7e7665a1ee4f6ea989f6b07a7dff6f709e65
Thibaut Narivelo <thibaut.randria@gmail.com>
2024-07-29 21:03:06 +0200

    feat: 🚧 Blog Page

--- app/(next)/blog/page.tsx
+++ app/(next)/blog/page.tsx
@@ -1,18 +1,71 @@
 import React from "react";
 
-import { sanityFetch } from "@/sanity/lib/fetch";
+import Image from "next/image";
 
+import { sanityFetch } from "@/sanity/lib/fetch";
 import { blogsQuery } from "@/sanity/lib/queries";
 import { BlogsQueryResult } from "@/sanity.types";
+
+import { DateHourFormat, MyCustomPortableText } from "../components/ui";
+import { PortableTextBlock } from "next-sanity";
+import { BlogDesktopSidebar } from "../components/blog";
 
 export default async function Page() {
   const [blogs] = await Promise.all([
     sanityFetch<BlogsQueryResult>({
       query: blogsQuery,
+      params: {
+        maxYear: new Date().getFullYear(),
+        minYear: new Date().getFullYear() - 4, // 5 years of blogs
+      },
     }),
   ]);
 
-  console.log("Blogs:", blogs);
+  // console.log("Blogs:", blogs);
 
-  return <div className="relative min-h-screen">Blog</div>;
+  return (
+    <div className="relative mx-auto mt-[3rem] min-h-screen pb-[1rem] lg:max-w-[1440px]">
+      <BlogDesktopSidebar blog={blogs} />
+      <div className="flex flex-col gap-[5rem] px-[1rem] lg:ml-arch lg:pr-[3rem]">
+        {blogs.map((blog) => (
+          <div
+            id={blog.year?.toString()}
+            key={blog._id}
+            className="flex flex-col justify-between border-t-[3px] border-black pb-[3rem]"
+          >
+            <h2 className="blogTitle">{blog.title}</h2>
+            <span className="blogSubTitle py-[.75rem]">{blog.subTitle}</span>
+            <span className="blogAuthor">{blog.author}</span>
+            <DateHourFormat
+              dateString={blog.date || ""}
+              className="blogDate pb-[2.5rem]"
+            />
+            {blog.contentBlock?.map((block, index) => {
+              if ("richText" in block) {
+                return (
+                  <MyCustomPortableText
+                    key={index}
+                    value={block.richText as PortableTextBlock[]}
+                    className="blogParagraph"
+                  />
+                );
+              }
+              if ("singleImage" in block) {
+                return (
+                  <Image
+                    key={index}
+                    src={block.singleImage.imageUrl || ""}
+                    alt={block.singleImage.alt || ""}
+                    width={1000}
+                    height={1000}
+                  />
+                );
+              }
+              return null;
+            })}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ app/(next)/components/blog/BlogDesktopSidebar.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React from "react";
+import clsx from "clsx";
+
+import { BlogsQueryResult } from "@/sanity.types";
+
+type Blog = {
+  _id: string;
+  year: number;
+  // other properties
+};
+
+type BlogDesktopSidebarProps = {
+  blog: BlogsQueryResult;
+};
+
+export default function BlogDesktopSidebar({ blog }: BlogDesktopSidebarProps) {
+  const [activeYear, setActiveYear] = React.useState<number | null>(
+    new Date().getFullYear(),
+  );
+  const handleBlogScroll = (e: React.MouseEvent<HTMLButtonElement>) => {
+    // step 1: set the active year
+    setActiveYear(parseInt(e.currentTarget.textContent || ""));
+
+    // step 2: scroll to the year
+    const year = e.currentTarget.textContent;
+    const element = document.getElementById(year || "");
+    if (element) {
+      const offset = 164; // Adjust this value to set the margin
+      const elementPosition =
+        element.getBoundingClientRect().top + window.pageYOffset;
+      const offsetPosition = elementPosition - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  // Filter to get only one blog per year
+  const uniqueYears = blog.reduce<Blog[]>((acc, current) => {
+    const x = acc.find((item) => item.year === current.year);
+    if (!x) {
+      const newBlog: Blog = {
+        _id: "",
+        year: current.year || 0,
+        // other properties
+      };
+      return acc.concat([newBlog]);
+    } else {
+      return acc;
+    }
+  }, []);
+
+  return (
+    <div className="fixed left-[calc(50%-720px)] top-[7.25rem] ml-[3.5rem] mt-[3rem] hidden flex-col gap-[1rem] lg:flex">
+      {uniqueYears.map((blog) => (
+        <div
+          key={blog._id}
+          className={clsx(
+            "sidebarButton",
+            activeYear === blog.year && "text-pink-arch",
+          )}
+        >
+          <button onClick={handleBlogScroll}>{blog.year}</button>
+        </div>
+      ))}
+    </div>
+  );
+}
