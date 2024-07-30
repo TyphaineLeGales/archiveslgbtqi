e07c631741062634bd472df944b7fe5e06c9b7e7
Thibaut Narivelo <thibaut.randria@gmail.com>
2024-07-30 18:31:27 +0200

    refactor: Update CookieBanner component to display cookie consent banner

--- app/(next)/components/ui/CookieBanner.tsx
+++ app/(next)/components/ui/CookieBanner.tsx
@@ -26,17 +26,37 @@ export default function CookieBanner() {
 
   return (
     <>
-      {/* {isFirstVisit && isVisible && ( */}
-      <div
-        className={clsx(
-          "fixed bottom-0 left-0 right-0 flex h-auto items-center justify-between bg-blue-200 p-[1rem] transition-transform duration-300 ease-tamisitée",
-          isVisible ? "translate-y-0" : "translate-y-full",
-        )}
-      >
-        <p>Cookie Banner</p>
-        <button onClick={handleClose}>x</button>
-      </div>
-      {/* )} */}
+      {isFirstVisit && isVisible && (
+        <div
+          className={clsx(
+            "fixed bottom-0 left-0 right-0 flex h-auto items-center justify-between bg-white p-[1rem] transition-transform duration-300 ease-tamisitée",
+            isVisible ? "translate-y-0" : "translate-y-full",
+          )}
+        >
+          <div className="flex flex-col items-center justify-between gap-[1rem] font-jetbrains text-[.8rem] leading-[1.2rem] tracking-wider lg:flex-row">
+            <p className="lg:max-w-[75%]">
+              En utilisant ce site, vous acceptez notre Politique de
+              Confidentialité mise à jour et notre Politique en matière de
+              cookies. Pour modifier vos préférences ou retirer votre
+              consentement, vous devez mettre à jour vos Préférences de cookies.
+              En savoir plus sur nos
+              <a
+                href="/mentions-legales"
+                className="px-[.6rem] uppercase text-pink-arch underline hover:no-underline"
+              >
+                Mentions légales et Politique de confidentialité
+              </a>
+              .
+            </p>
+            <button
+              className="heroCta aspect-auto w-full bg-black p-[1rem] hover:text-pink-arch lg:aspect-square lg:w-auto"
+              onClick={handleClose}
+            >
+              Ok
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
