fdb6e4c88537e84c9f349f76d78104e263506a95
Typhaine Le Gales <typhaineLeGales@protonmail.com>
2025-02-11 11:03:17 +0100

    chore: type props to prevent build fail

--- app/(next)/components/modules/FileModule.tsx
+++ app/(next)/components/modules/FileModule.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-
-const FileModule = ({ item }) => {
+type DocFile = {
+  fileUrl: string
+ 
+};
+const FileModule = ({ item }: DocFile) => {
 
   return item.fileUrl && <iframe src={item.fileUrl} width="100%" height="700px"></iframe>
 
